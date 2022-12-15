428a843fa858a2685195c4cdd5cf56572d70dbaa
Shamanesss <mentulbenodorna@gmail.com>
2022-12-15 21:19:23 +0100

    pagina actualidad con header y footer

---
+++ slider.js
@@ -0,0 +1,11 @@
+$(document).ready(function(){
+
+    //slider
+  $('#noticias_slider').bxSlider({
+    mode: 'fade',
+    captions: true,
+    slideWidth: 1500
+  });
+
+  $("#noticias_click").trigger("click");
+});
