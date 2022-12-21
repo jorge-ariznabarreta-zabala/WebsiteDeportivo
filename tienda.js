f5b90a2b9bfdfa7e6067ce66f162b8cf26da5757
jorge-ariznabarreta-zabala <jorge.ariznabarreta.zabala@gmail.com>
2022-12-21 10:47:51 +0100

    carritoFinal

--- tienda.js
+++ tienda.js
@@ -1,15 +1,16 @@
 const precios= {'Bermuda con bolsillo':30, "Camiseta entrenamiento":33, "Chaqueta chandal":53, "Pantalón":53, "Polo":33, "Sudadera":53, "Camiseta Primera Equipacion":63, "Camiseta Segunda Equipacion":63, "Bidón":13, "Bufanda":18, "Llavero":8, "Paraguas":18, "Taza" :13, "Zapatillas":23};
 const pedido={"Bermuda con bolsillo":0, "Camiseta entrenamiento":0, "Chaqueta chandal":0, "Pantalón":0, "Polo":0, "Sudadera":0, "Camiseta Primera Equipacion":0, "Camiseta Segunda Equipacion":0, "Bidón":0, "Bufanda":0, "Llavero":0, "Paraguas":0, "Taza" :0, "Zapatillas":0};
 const subtotales={"Bermuda con bolsillo":0, "Camiseta entrenamiento":0, "Chaqueta chandal":0, "Pantalón":0, "Polo":0, "Sudadera":0, "Camiseta Primera Equipacion":0, "Camiseta Segunda Equipacion":0, "Bidón":0, "Bufanda":0, "Llavero":0, "Paraguas":0, "Taza" :0, "Zapatillas":0};
+
 function pedirProducto(nombre){
     // console.log(pedido);
     pedido[nombre] = pedido[nombre] +1;
     // console.log(pedido);
     escribir();
 };
+
 function escribir(){
 let totalPedido=0;
-console.log(pedido.length);
 for (const key in pedido) {
     let auxiliar= precios[key]*pedido[key];
     totalPedido += auxiliar;
