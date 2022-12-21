495ad9ffc99b631b654374863f18633e29e85f2f
Zguedest <zucelys.zge@gmail.com>
2022-12-21 09:36:37 +0100

    aumentando velocidad del banner

--- home.js
+++ home.js
@@ -3,10 +3,10 @@ var imgs = [];
 var tiempo = 1500;
 
 //lista de imagenes
-imgs[0] = "https://sdamorebieta.eus/wp-content/uploads/2022/12/alevin-11a-r-1200x900.jpg";
-imgs[1] = "https://sdamorebieta.eus/wp-content/uploads/2022/12/81909584-BA16-4FE0-A88E-7EBB8729066E-1200x850.jpeg";
-imgs[2] = "https://sdamorebieta.eus/wp-content/uploads/2022/12/F1338BA8-B591-44C4-8953-663A495E7EF6-1200x800.jpeg";
-imgs [3]= "./img/header-image.png";
+imgs[0] = "./img/imagen_header1.webp";
+imgs[1] = "./img/imagen_header2.webp";
+imgs[2] = "./img/imagen_header3.webp";
+imgs [3]= "./img/header-image.webp";
 
 function cambiarImagenHeader() {
 
